66977966684368628fff5677f902935c4d09a71e
yiheyoung <yiheyoung@gmail.com>
2026-01-05 21:45:00 +0800

    initial setup for log system

--- GameEngine/src/GameEngine.h
+++ GameEngine/src/GameEngine.h
@@ -3,6 +3,7 @@
 // For use by GameEngine applications
 
 #include "GameEngine/Application.h"
+#include "GameEngine/Log.h"
 
 // ---Entry Point-----------------
 #include "GameEngine/EntryPoint.h"
---
+++ GameEngine/src/GameEngine/Log.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+#include <spdlog/sinks/stdout_color_sinks.h>
+
+namespace GameEngine
+{
+	class GAMEENGINE_API Log
+	{
+	public:
+		static void Init();
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+// Core log macros
+#define GE_CORE_TRACE(...)    ::GameEngine::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define GE_CORE_INFO(...)     ::GameEngine::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define GE_CORE_WARN(...)     ::GameEngine::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define GE_CORE_ERROR(...)    ::GameEngine::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define GE_CORE_CRITICAL(...) ::GameEngine::Log::GetCoreLogger()->critical(__VA_ARGS__)
+
+// Client log macros
+#define GE_TRACE(...)         ::GameEngine::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define GE_INFO(...)          ::GameEngine::Log::GetClientLogger()->info(__VA_ARGS__)
+#define GE_WARN(...)          ::GameEngine::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define GE_ERROR(...)         ::GameEngine::Log::GetClientLogger()->error(__VA_ARGS__)
+#define GE_CRITICAL(...)	  ::GameEngine::Log::GetClientLogger()->critical(__VA_ARGS__)

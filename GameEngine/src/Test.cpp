dc20abb9471467e2adf1769cfd0d3fa45b90664f
yiheyoung <yiheyoung@gmail.com>
2026-01-05 14:22:10 +0800

    initial setup

---
+++ GameEngine/src/Test.cpp
@@ -0,0 +1,9 @@
+#include "Test.h"
+#include <iostream>
+
+namespace GameEngine {
+	void Print()
+	{
+		std::cout << "Hello from GameEngine!" << std::endl;
+	}
+}
---
+++ Sandbox/src/Application.cpp
@@ -0,0 +1,8 @@
+namespace GameEngine {
+	__declspec(dllimport) void Print();
+}
+
+void main()
+{
+	GameEngine::Print();
+}

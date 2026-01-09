833df734506777e09e461e2edc7c482c3e2e306f
yiheyoung <yiheyoung@gmail.com>
2026-01-09 11:19:53 +0800

    set glfw callback for window events

--- GameEngine/src/GameEngine/Application.cpp
+++ GameEngine/src/GameEngine/Application.cpp
@@ -2,19 +2,31 @@
 #include "Application.h"
 
 #include "Events/Event.h"
-#include "GameEngine/Events/ApplicationEvent.h"
 #include "GameEngine/Log.h"
 
+#include <GLFW/glfw3.h>
+
 namespace GameEngine {
+#define BIND_EVENT_FN(fn) std::bind(&Application::fn, this, std::placeholders::_1)
 	Application::Application()
 	{
 		// create the window using the Window class's static Create method
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+
+		GE_CORE_TRACE("{0}", e);
+	}
+
 	void Application::Run()
 	{
 		WindowResizeEvent event(1280, 720);
@@ -28,7 +40,15 @@ namespace GameEngine {
 		}
 		while (m_Running)
 		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
 	}
 }
--- GameEngine/src/GameEngine/Application.h
+++ GameEngine/src/GameEngine/Application.h
@@ -3,13 +3,19 @@
 #include "Core.h"
 #include "Window.h"
 
+#include "GameEngine/Events/ApplicationEvent.h"
+
 namespace GameEngine {
 	class GAMEENGINE_API Application {
 	public:
 		Application();
 		virtual ~Application();
+
+		void OnEvent(Event& e); // Event handler
 		void Run();
 	private:
+		bool OnWindowClose(WindowCloseEvent& e); // Window close event handler
+
 		std::unique_ptr<Window> m_Window;    // unique_ptr to manage window memory
 		bool m_Running = true;
 	};

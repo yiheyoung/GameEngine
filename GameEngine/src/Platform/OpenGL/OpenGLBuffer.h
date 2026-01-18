7937e6cdabc43dbea82004280a4d6e952fbfd4eb
yiheyoung <yiheyoung@gmail.com>
2026-01-18 16:18:39 +0800

    add buffer layout for vertex buffers

--- GameEngine/src/Platform/OpenGL/OpenGLBuffer.h
+++ GameEngine/src/Platform/OpenGL/OpenGLBuffer.h
@@ -10,8 +10,12 @@ namespace GameEngine {
 		virtual ~OpenGLVertexBuffer();
 		virtual void Bind() const override;
 		virtual void Unbind() const override;
+
+		virtual const BufferLayout& GetLayout() const override { return m_Layout; }
+		virtual void SetLayout(const BufferLayout& layout) override { m_Layout = layout; }
 	private:
 		uint32_t m_RendererId;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer

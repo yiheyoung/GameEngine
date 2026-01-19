658dadddf5577879f5ec4f45c8ec8374b2e58844
yiheyoung <yiheyoung@gmail.com>
2026-01-19 15:05:52 +0800

    delete redundant file

--- GameEngine/RenderCommand.cpp
+++
@@ -1,8 +0,0 @@
-#include "gepch.h"
-#include "RenderCommand.h"
-
-#include "Platform/OpenGL/OpenGLRendererAPI.h"
-
-namespace GameEngine {
-	RendererAPI* RenderCommand::s_RendererAPI = new OpenGLRendererAPI();
-}

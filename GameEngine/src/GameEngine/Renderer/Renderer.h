6ba2f0f68a621d2f89dd0ea06ffce9a27a07c2b0
yiheyoung <yiheyoung@gmail.com>
2026-01-23 19:51:11 +0800

    add transforms to Renderer

--- GameEngine/src/GameEngine/Renderer/Renderer.h
+++ GameEngine/src/GameEngine/Renderer/Renderer.h
@@ -12,7 +12,7 @@ namespace GameEngine {
 		static void BeginScene(OrthographicCamera& camera);
 		static void EndScene();
 
-		static void Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray);
+		static void Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform);
 
 		inline static RendererAPI::API GetAPI() { return RendererAPI::GetAPI(); }
 	private:
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,6 +1,7 @@
 #include "GameEngine.h"
 
 #include "imgui/imgui.h"
+#include <glm/gtc/matrix_transform.hpp>
 
 class ExampleLayer : public GameEngine::Layer
 {
@@ -35,10 +36,10 @@ public:
 		m_SquareVA.reset(GameEngine::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f
 		};
 
 		std::shared_ptr<GameEngine::VertexBuffer> squareVB;
@@ -95,13 +96,14 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -144,9 +146,18 @@ public:
 
 		GameEngine::Renderer::BeginScene(m_Camera);
 
-		GameEngine::Renderer::Submit(m_BlueShader, m_SquareVA);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
+				GameEngine::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
 
-		GameEngine::Renderer::Submit(m_Shader, m_VertexArray);
+		GameEngine::Renderer::Submit(m_Shader, m_VertexArray, glm::mat4(1.0f));
 
 		GameEngine::Renderer::EndScene();
 	}

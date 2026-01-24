f418665574726046c8679a6b71883dc83249574f
yiheyoung <yiheyoung@gmail.com>
2026-01-24 14:42:07 +0800

    refactor smart pointer types to our own type

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -21,7 +21,7 @@ public:
 			 0.0f,  0.5f, 0.0f, 0.3f, 0.8f, 0.2f, 1.0f
 		};
 
-		std::shared_ptr<GameEngine::VertexBuffer> vertexBuffer;
+		GameEngine::Ref<GameEngine::VertexBuffer> vertexBuffer;
 		vertexBuffer.reset(GameEngine::VertexBuffer::Create(vertices, sizeof(vertices)));
 		GameEngine::BufferLayout layout = {
 			{ GameEngine::ShaderDataType::Float3, "a_Position" },
@@ -31,7 +31,7 @@ public:
 		m_VertexArray->AddVertexBuffer(vertexBuffer);
 
 		uint32_t indices[3] = { 0, 1, 2 };
-		std::shared_ptr<GameEngine::IndexBuffer> indexBuffer;
+		GameEngine::Ref<GameEngine::IndexBuffer> indexBuffer;
 		indexBuffer.reset(GameEngine::IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
 		m_VertexArray->SetIndexBuffer(indexBuffer);
 
@@ -45,7 +45,7 @@ public:
 			-0.5f,  0.5f, 0.0f
 		};
 
-		std::shared_ptr<GameEngine::VertexBuffer> squareVB;
+		GameEngine::Ref<GameEngine::VertexBuffer> squareVB;
 		squareVB.reset(GameEngine::VertexBuffer::Create(squareVertices, sizeof(squareVertices)));
 		GameEngine::BufferLayout squareLayout = {
 			{ GameEngine::ShaderDataType::Float3, "a_Position" }
@@ -54,7 +54,7 @@ public:
 		m_SquareVA->AddVertexBuffer(squareVB);
 
 		uint32_t squareIndices[6] = { 0, 1, 2, 2, 3, 0 };
-		std::shared_ptr<GameEngine::IndexBuffer> squareIB;
+		GameEngine::Ref<GameEngine::IndexBuffer> squareIB;
 		squareIB.reset(GameEngine::IndexBuffer::Create(squareIndices, sizeof(squareIndices) / sizeof(uint32_t)));
 		m_SquareVA->SetIndexBuffer(squareIB);
 
@@ -183,11 +183,11 @@ public:
 		// Event handling logic for the layer
 	}
 private:
-	std::shared_ptr<GameEngine::Shader> m_Shader;
-	std::shared_ptr<GameEngine::VertexArray> m_VertexArray;
+	GameEngine::Ref<GameEngine::Shader> m_Shader;
+	GameEngine::Ref<GameEngine::VertexArray> m_VertexArray;
 
-	std::shared_ptr<GameEngine::Shader> m_FlatColorShader;
-	std::shared_ptr<GameEngine::VertexArray> m_SquareVA;
+	GameEngine::Ref<GameEngine::Shader> m_FlatColorShader;
+	GameEngine::Ref<GameEngine::VertexArray> m_SquareVA;
 
 	GameEngine::OrthographicCamera m_Camera;
 
